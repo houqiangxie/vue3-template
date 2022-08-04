bfd9a58441db64f2d9892691976ce7b4029c9868
houqiang.xie <1624539850@qq.com>
2022-08-04 11:03:47 +0800

    更新pdf组件

--- src/views/web/home/HomeIndex.vue
+++ src/views/web/home/HomeIndex.vue
@@ -4,11 +4,11 @@
  * @Author: houqiangxie
  * @Date: 2022-05-24 10:57:49
  * @LastEditors: houqiangxie
- * @LastEditTime: 2022-06-07 14:54:57
+ * @LastEditTime: 2022-08-04 11:03:10
 -->
 <template>
     <div>
-        <img src="@/assets/images/main.png" alt="" class="w-full h-40">
+        <PDF :pdfUrl="'/myResource/d78f9068-b5ec-49ed-b826-c27e6b4469eb.pdf'" />
     </div>
 </template>
 
